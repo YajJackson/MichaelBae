983adef5033caacfaafeab546ea445cec71abc67
Jay Nicholas Jackson <jaynickjackson@gmail.com>
2017-06-28 11:57:42 -0400

    create a flick object with a name and id

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+// this is bound to the target of the object
 const app = {
     init: function(formSelector){
+        this.max = 0
         document
             .querySelector(formSelector)
-            .addEventListener('submit', this.handleSubmit) //this refers to the object itself
+            .addEventListener('submit', this.handleSubmit.bind(this)) //rebinds 'this' to the function that is called
     },
     
     handleSubmit(ev){
         ev.preventDefault()
-        const flickName = ev.target.flickName.value
-        console.log(flickName)
+        const f = ev.target
+        
+        const flick = {
+            id: this.max + 1,
+            name: f.flickName.value,
+        }
+
+        this.max ++
+        console.log(flick.name, this.max)
     },
 }
 
