dbb461a88924b50c7805eec188520cb7233ebd21
Jay Nicholas Jackson <jaynickjackson@gmail.com>
2017-06-29 09:05:02 -0400

    attempted homeowrk 3 bonus 4, but could not get move up and down to work properly

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # MichaelBae
 
  A catalog of Michael Bay's extensive filmography.
+
+## Day 3 homework
+
+* store the elements in an array, as well as in the DOM
+
+# Bonus 1
+
+* Add a _fav_ button to each list item that changes the appearance of that item, for example, change background color //one-liner
+
+# Bonus 2
+
+* Add a _remove_ button to each list item that works
+
+# Bonus 3
+
+* Make sure that both of those buttons affect the array as well
+* Make _fav_ button toggle the favorite status
+
+# Bonus 4
+
+* Add buttons that move a flick up and down the list. //reorder array as well
--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 // this is bound to the target of the object
 const app = {
     init: function(selectors){
+        this.flicks = []
         this.max = 0
         this.list = document.querySelector(selectors.listSelector)
         document
             .querySelector(selectors.formSelector)
             .addEventListener('submit', this.handleSubmit.bind(this)) //rebinds 'this' to the function that is called
-    },
-
-    renderListItem(flick) {
-        const item =document.createElement('li')
-        item.textContent = flick.name
-        return item
     },
     
     handleSubmit(ev){
@@ -20,12 +15,84 @@ const app = {
         const flick = {
             id: this.max + 1,
             name: f.flickName.value,
+            opinion: null,
+
         }
 
         const listItem = this.renderListItem(flick)
+
+        this.flicks.push(flick)
         this.list.appendChild(listItem)
         this.max ++
     },
+
+    renderListItem(flick) {
+        // create list item
+        const item = document.createElement('li')
+        item.className = 'list-group-item'
+        item.id = `listItem${flick.id}`
+        item.arrayId = flick.id
+        item.textContent = flick.name
+
+        //create button group
+        item.appendChild(this.addButtonGroup(flick.id))
+        // console.log(this.flicks)
+        return item
+    },
+
+    addButtonGroup(id) {
+        const group = document.createElement('span')
+        group.className = 'button-group'
+        group.appendChild(this.addButton('like', id))
+        group.appendChild(this.addButton('remove', id))
+        group.appendChild(this.addButton('up', id))
+        group.appendChild(this.addButton('down', id))
+        return group
+    },
+
+    addButton(buttonName, id) {
+        const button = document.createElement('li')
+        button.className = 'button'
+        button.id = `${buttonName}Button${id}`
+        button.type = `${buttonName}`
+        button.arrayId = id
+        button.textContent = `${buttonName}`
+        button.addEventListener('click', this.handleClick)
+        return button
+    },
+
+    handleClick() {
+        // console.log(this.arrayId)
+        buttonId = `${this.id}`
+        listId = `#listItem${this.arrayId}`
+        clickType = this.type
+        switch(clickType) {
+            case 'like':
+                document.querySelector(`#${buttonId}`).style.backgroundColor = 'lightGreen'
+                break
+            case 'remove':
+                document.querySelector(listId).remove()
+                break
+            case 'up':
+                app.moveUp(listId, this.arrayId)
+                break
+            case 'down':
+                app.moveDown(listId, this.arrayId)
+                break
+        }
+    },
+
+    moveUp(item, arrayId){
+        const startingItem = document.querySelector(`${item}`)
+        const newItem = document.querySelector(`#listItem${arrayId-1}`)
+        console.log(document.querySelector('#flick-list').children[arrayId-1])
+        document.querySelector('#flick-list').insertBefore(document.querySelector(`#listItem${arrayId-1}`), document.querySelector('#flick-list').children[arrayId-1])
+        // console.log(startingItem, newItem, item, arrayId)
+    },
+    moveDown(item, arrayId){
+        console.log(item, arrayId)
+    },
+
 }
 
 app.init({
