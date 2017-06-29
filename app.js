9c181642421e7b96ed96e82ce30ecb2d0efba91b
Jay Nicholas Jackson <jaynickjackson@gmail.com>
2017-06-29 12:30:51 -0400

    fixed markup

--- app.js
+++ app.js
@@ -71,60 +71,7 @@ const app = {
             )
         return item
     },
-
-    addButtonGroup(id) {
-        const group = document.createElement('span')
-        group.className = 'button-group'
-        group.appendChild(this.addButton('like', id))
-        group.appendChild(this.addButton('remove', id))
-        group.appendChild(this.addButton('up', id))
-        group.appendChild(this.addButton('down', id))
-        return group
-    },
-
-    addButton(buttonName, id) {
-        const button = document.createElement('li')
-        button.className = 'button'
-        button.id = `${buttonName}Button${id}`
-        button.type = `${buttonName}`
-        button.arrayId = id
-        button.textContent = `${buttonName}`
-        button.addEventListener('click', this.handleClick)
-        return button
-    },
-
-    handleClick() {
-        // console.log(this.arrayId)
-        buttonId = `${this.id}`
-        listId = `#listItem${this.arrayId}`
-        clickType = this.type
-        switch(clickType) {
-            case 'like':
-                document.querySelector(`#${buttonId}`).style.backgroundColor = 'lightGreen'
-                break
-            case 'remove':
-                document.querySelector(listId).remove()
-                break
-            case 'up':
-                app.moveUp(listId, this.arrayId)
-                break
-            case 'down':
-                app.moveDown(listId, this.arrayId)
-                break
-        }
-    },
-
-    moveUp(item, arrayId){
-        const startingItem = document.querySelector(`${item}`)
-        const newItem = document.querySelector(`#listItem${arrayId-1}`)
-        console.log(document.querySelector('#flick-list').children[arrayId-1])
-        document.querySelector('#flick-list').insertBefore(document.querySelector(`#listItem${arrayId-1}`), document.querySelector('#flick-list').children[arrayId-1])
-        // console.log(startingItem, newItem, item, arrayId)
-    },
-    moveDown(item, arrayId){
-        console.log(item, arrayId)
-    },
-
+    
 }
 
 app.init({
